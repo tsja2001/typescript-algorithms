b5f8d33b26accb9a757756d227c9d0c2c18baefa
tsja2001 <52786405+tsja2001@users.noreply.github.com>
2023-03-14 14:13:28 +0800

    feat: 完成队列

---
+++ src/02_queue/__test__/02_约瑟夫环.test.ts
@@ -0,0 +1,9 @@
+import { lastRemaining } from './../example/02_约瑟夫环';
+import { Queue } from '../lib/01_创建queue';
+
+describe('约瑟夫环', () => {
+	it('约瑟夫环', () => {
+		expect(lastRemaining(5, 3)).toBe(3)
+		expect(lastRemaining(10, 17)).toBe(2)
+	})
+});
---
+++ src/02_queue/example/02_约瑟夫环.ts
@@ -0,0 +1,31 @@
+import { Queue } from './../lib/01_创建queue'
+/**
+ * https://leetcode.cn/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof/
+ */
+
+/**
+ *
+ * @param n 总人数
+ * @param m 一次跳过多少人
+ */
+export const lastRemaining = (n: number, m: number) => {
+  const queue = new Queue<number>()
+
+  for (let i = 0; i < n; i++) {
+    queue.enQuque(i)
+  }
+
+  while (queue.size() > 1) {
+    for (let i = 1; i < m; i++) {
+      queue.enQuque(queue.deQuque()!)
+    }
+
+    queue.deQuque()
+  }
+
+	return queue.deQuque()!
+}
+
+
+const res = lastRemaining(5, 3)
+console.log(res)
