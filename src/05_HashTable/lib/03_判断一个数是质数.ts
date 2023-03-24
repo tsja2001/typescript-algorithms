fcde3e3c167217ec9ea883a1c981317d8da8f86d
tsja2001 <52786405+tsja2001@users.noreply.github.com>
2023-03-24 16:26:41 +0800

    feat: 增加判断质数算法

---
+++ src/05_HashTable/lib/03_判断一个数是质数.ts
@@ -0,0 +1,9 @@
+export const isPrime = (num: number): boolean => {
+  // 获取平方根
+  const square = Math.sqrt(num)
+  for (let i = 2; i < square; i++) {
+    if (num % i === 0) return false
+  }
+
+  return true
+}
